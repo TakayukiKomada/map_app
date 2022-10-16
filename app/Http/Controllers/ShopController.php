3d7ba3180dcdb134c59ae08cfad7cee4bcf3bbbb
TakayukiKomada <Takakoma.79@gmail.com>
2022-10-16 11:09:37 +0900

    CRUD処理の修正

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\Shop;
 use Illuminate\Http\Request;
+
 class ShopController extends Controller
 {
     /**
@@ -11,7 +14,8 @@ class ShopController extends Controller
      */
     public function index()
     {
-        return view('shops.index');
+        $shops = Shop::all();
+        return view('shops.index', compact('shops'));
     }
     /**
      * Show the form for creating a new resource.
@@ -30,7 +34,16 @@ class ShopController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $shop = new Shop();
+
+        $shop->name = $request->name;
+        $shop->description = $request->description;
+        $shop->address = $request->address;
+        $shop->latitude = $request->latitude;
+        $shop->longitude = $request->longitude;
+        $shop->save();
+
+        return redirect()->route('shops.index');
     }
     /**
      * Display the specified resource.
@@ -40,7 +53,7 @@ class ShopController extends Controller
      */
     public function show(Shop $shop)
     {
-        return view('shops.show');
+        return view('shops.show', compact('shop'));
     }
     /**
      * Show the form for editing the specified resource.
@@ -50,7 +63,7 @@ class ShopController extends Controller
      */
     public function edit(Shop $shop)
     {
-        return view('shops.edit');
+        return view('shops.edit', compact('shop'));
     }
     /**
      * Update the specified resource in storage.
@@ -61,7 +74,14 @@ class ShopController extends Controller
      */
     public function update(Request $request, Shop $shop)
     {
-        //
+        $shop->name = $request->name;
+        $shop->description = $request->description;
+        $shop->address = $request->address;
+        $shop->latitude = $request->latitude;
+        $shop->longitude = $request->longitude;
+        $shop->save();
+
+        return redirect()->route('shops.index');
     }
     /**
      * Remove the specified resource from storage.
@@ -71,6 +91,7 @@ class ShopController extends Controller
      */
     public function destroy(Shop $shop)
     {
-        //
+        $shop->delete();
+        return redirect()->route('shops.index');
     }
 }
